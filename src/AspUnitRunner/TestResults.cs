fafc3cf34426c4cec7ac197fc34e7681aeeb9a76
Mike Henry <mhenry07@yahoo.com>
2009-05-08 19:15:54 -0500

    Added ASPUnitRunner source.
    Contains two classes: TestRunner and TestResults.

---
+++ src/AspUnitRunner/TestResults.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AspUnitRunner {
+    public class TestResults {
+        public int Tests { get; private set; }
+        public int Errors { get; private set; }
+        public int Failures { get; private set; }
+        public string Results { get; private set; }
+
+        public bool IsSuccessful {
+            get {
+                return (Errors == 0 && Failures == 0);
+            }
+        }
+
+        public TestResults(string htmlResults) {
+            Results = htmlResults;
+            Tests = ParseCount(@"Tests\: (\d+)", htmlResults);
+            Errors = ParseCount(@"Errors\: (\d+)", htmlResults);
+            Failures = ParseCount(@"Failures\: (\d+)", htmlResults);
+        }
+
+        // pattern is a Regex pattern where the first capturing group is the number to parse
+        private int ParseCount(string pattern, string htmlResults) {
+            Regex regex = new Regex(pattern);
+            foreach (Match match in regex.Matches(htmlResults))
+                return int.Parse(match.Groups[1].Value);
+            return 0;
+        }
+
+        public string FormatResults() {
+            return Regex.Replace(Results, @"(\s*<TR)", "\r\n$1", RegexOptions.IgnoreCase);
+        }
+    }
+}
---
+++ src/AspUnitRunner/TestRunner.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace AspUnitRunner {
+    public class TestRunner {
+        private const string BaseQueryString = "?UnitRunner=results";
+        private const string DefaultTestCases = "All Test Cases";
+        private const string RunValue = "Run Tests";
+
+        private string _baseUri;
+        private ICredentials _credentials;
+
+        // Note: For default credentials to work the test directory must have Integrated Windows Authentication enabled.
+        public TestRunner(string baseUri) : this(baseUri, CredentialCache.DefaultCredentials) { }
+        
+        public TestRunner(string baseUri, ICredentials credentials) {
+            _baseUri = baseUri;
+            _credentials = credentials;
+        }
+
+        public TestResults Run(string testContainer) {
+            WebRequest request = WebRequest.Create(_baseUri + BaseQueryString);
+            request.Method = "POST";
+            request.Credentials = _credentials;
+
+            string postData = String.Format("cboTestContainers={0}&cboTestCases={1}&cmdRun={2}", 
+                HttpUtility.UrlEncode(testContainer), HttpUtility.UrlEncode(DefaultTestCases), HttpUtility.UrlEncode(RunValue));
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] postBytes = encoding.GetBytes(postData);
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = postBytes.Length;
+            using (Stream requestStream = request.GetRequestStream()) {
+                requestStream.Write(postBytes, 0, postBytes.Length);
+            }
+
+            using (WebResponse response = request.GetResponse()) {
+                using (StreamReader responseStream = new StreamReader(response.GetResponseStream())) {
+                    return new TestResults(responseStream.ReadToEnd());
+                }
+            }
+        }
+    }
+}
