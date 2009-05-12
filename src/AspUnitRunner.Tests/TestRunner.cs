2593dbce1facf2f48f324ef21c97199617a79260
Mike Henry <mhenry07@yahoo.com>
2009-05-11 20:37:39 -0500

    Restarted AspUnitRunner so I can rewrite it using TDD.

---
+++ src/AspUnitRunner.Tests/TestRunner.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace AspUnitRunner.Tests {
+    [TestFixture]
+    public class TestRunner {
+
+    }
+}
