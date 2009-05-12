5d4fcbd4ed61baed60a5d8e6b23b82a8ec7023b6
Mike Henry <mhenry07@yahoo.com>
2009-05-12 00:20:43 -0500

    Got Erroneous_test_should_return_an_error to pass.
    Got test to pass by updating Results.ParseResults to handle Errors.

--- src/AspUnitRunner/Results.cs
+++ src/AspUnitRunner/Results.cs
@@ -13,8 +13,9 @@ namespace AspUnitRunner {
         }
 
         private void ParseResults(string htmlResults) {
-            Regex regex = new Regex(@"Failures\:\s*(?<failures>\d+)");
+            Regex regex = new Regex(@"Errors\:\s*(?<errors>\d+),\s*Failures\:\s*(?<failures>\d+)");
             foreach (Match match in regex.Matches(htmlResults)) {
+                Errors = int.Parse(match.Groups["errors"].Value);
                 Failures = int.Parse(match.Groups["failures"].Value);
             }
         }
