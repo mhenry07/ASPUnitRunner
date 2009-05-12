73e13011e80b952d5ba41f56fc92b5744e637806
Mike Henry <mhenry07@yahoo.com>
2009-05-11 20:58:47 -0500

    Added a new failing test Should_run_tests.
    Added a new class Runner with a Run method.

--- src/AspUnitRunner.Tests/TestRunner.cs
+++ src/AspUnitRunner.Tests/TestRunner.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 using NUnit.Framework;
+using AspUnitRunner;
 
 namespace AspUnitRunner.Tests {
     [TestFixture]
     public class TestRunner {
-
+        [Test]
+        public void Should_run_tests() {
+            Runner runner = new Runner();
+            Assert.That(runner.Run(), Is.True);
+        }
     }
 }
---
+++ src/AspUnitRunner/Runner.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspUnitRunner {
+    public class Runner {
+        public bool Run() {
+            return false;
+        }
+    }
+}
