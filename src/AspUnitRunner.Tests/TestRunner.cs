eeca53b83508edac903d0175efe451639016d169
Mike Henry <mhenry07@yahoo.com>
2009-05-12 21:34:09 -0500

    Extracted fake proxy setup code to Setup.

--- src/AspUnitRunner.Tests/TestRunner.cs
+++ src/AspUnitRunner.Tests/TestRunner.cs
@@ -7,30 +7,32 @@ using AspUnitRunner;
 namespace AspUnitRunner.Tests {
     [TestFixture]
     public class TestRunner {
+        private FakeProxy _fakeProxy;
+
+        [SetUp]
+        public void Setup() {
+            _fakeProxy = new FakeProxy();
+            _fakeProxy.HtmlResults = FormatTestSummary(1, 0, 0);
+        }
+
         [Test]
         public void Running_tests_should_return_results() {
-            FakeProxy fakeProxy = new FakeProxy();
-            fakeProxy.HtmlResults = FormatTestSummary(1, 0, 0);
-            Runner runner = new Runner("", fakeProxy);
+            Runner runner = new Runner("", _fakeProxy);
             Assert.That(runner.Run(""), Is.TypeOf<Results>());
         }
 
         [Test]
         public void Should_pass_expected_uri_to_proxy() {
-            FakeProxy fakeProxy = new FakeProxy();
-            fakeProxy.HtmlResults = FormatTestSummary(1, 0, 0);
-            Runner runner = new Runner("http://path/to/test-runner", fakeProxy);
+            Runner runner = new Runner("http://path/to/test-runner", _fakeProxy);
             Results results = runner.Run("");
-            Assert.That(fakeProxy.Uri, Is.EqualTo("http://path/to/test-runner?UnitRunner=results"));
+            Assert.That(_fakeProxy.Uri, Is.EqualTo("http://path/to/test-runner?UnitRunner=results"));
         }
 
         [Test]
         public void Should_pass_expected_data_to_proxy() {
-            FakeProxy fakeProxy = new FakeProxy();
-            fakeProxy.HtmlResults = FormatTestSummary(1, 0, 0);
-            Runner runner = new Runner("http://path/to/test-runner", fakeProxy);
+            Runner runner = new Runner("http://path/to/test-runner", _fakeProxy);
             Results results = runner.Run("TestContainer");
-            Assert.That(fakeProxy.PostData, Is.EqualTo("cboTestContainers=TestContainer&cboTestCases=All+Test+Cases&cmdRun=Run+Tests"));
+            Assert.That(_fakeProxy.PostData, Is.EqualTo("cboTestContainers=TestContainer&cboTestCases=All+Test+Cases&cmdRun=Run+Tests"));
         }
 
         public static string FormatTestSummary(int tests, int errors, int failures) {
