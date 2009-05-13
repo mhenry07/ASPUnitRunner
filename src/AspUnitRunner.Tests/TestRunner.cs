fed3907667546bec940507844fa45c6108501875
Mike Henry <mhenry07@yahoo.com>
2009-05-12 19:31:19 -0500

    Implemented Proxy.GetTestResults and updated tests to inject the fake proxy.
    Also extracted a test helper method FormatTestSummary.

--- src/AspUnitRunner.Tests/TestRunner.cs
+++ src/AspUnitRunner.Tests/TestRunner.cs
@@ -9,13 +9,18 @@ namespace AspUnitRunner.Tests {
     public class TestRunner {
         [Test]
         public void Running_tests_should_return_results() {
-            Runner runner = new Runner();
+            FakeProxy fakeProxy = new FakeProxy();
+            fakeProxy.HtmlResults = FormatTestSummary(1, 0, 0);
+            Runner runner = new Runner(fakeProxy);
             Assert.That(runner.Run(), Is.TypeOf<Results>());
         }
 
         [Test]
         public void Passing_tests_should_return_no_errors_or_failures() {
-            Runner runner = new Runner();
+            FakeProxy fakeProxy = new FakeProxy();
+            fakeProxy.HtmlResults = FormatTestSummary(1, 0, 0);
+
+            Runner runner = new Runner(fakeProxy);
             Results results = runner.Run();
             Assert.That(results.Errors, Is.EqualTo(0));
             Assert.That(results.Failures, Is.EqualTo(0));
@@ -24,7 +29,7 @@ namespace AspUnitRunner.Tests {
         [Test]
         public void Failing_test_should_return_a_failure() {
             FakeProxy fakeProxy = new FakeProxy();
-            fakeProxy.HtmlResults = "<html><body><table><tr>Tests: 1, Errors: 0, Failures: 1</tr></table></body></html>";
+            fakeProxy.HtmlResults = FormatTestSummary(1, 0, 1);
 
             Runner runner = new Runner(fakeProxy);
             Results results = runner.Run();
@@ -34,11 +39,16 @@ namespace AspUnitRunner.Tests {
         [Test]
         public void Erroneous_test_should_return_an_error() {
             FakeProxy fakeProxy = new FakeProxy();
-            fakeProxy.HtmlResults = "<html><body><table><tr>Tests: 1, Errors: 1, Failures: 0</tr></table></body></html>";
+            fakeProxy.HtmlResults = FormatTestSummary(1, 1, 0);
 
             Runner runner = new Runner(fakeProxy);
             Results results = runner.Run();
             Assert.That(results.Errors, Is.EqualTo(1));
+        }
+
+        private string FormatTestSummary(int tests, int errors, int failures) {
+            return String.Format("<html><body><table><tr>Tests: {0}, Errors: {1}, Failures: {2}</tr></table></body></html>",
+                tests, errors, failures);
         }
     }
 }
--- src/AspUnitRunner/Proxy.cs
+++ src/AspUnitRunner/Proxy.cs
@@ -5,9 +5,32 @@ using System.Net;
 using System.IO;
 
 namespace AspUnitRunner {
+    // Note: this class isn't under test (it depends on WebRequest and WebResponse which are hard to fake).
     class Proxy : IProxy {
         public string GetTestResults(string url, string postData) {
-            return "";
+            WebRequest request = WebRequest.Create(url);
+            request.Method = WebRequestMethods.Http.Post;
+            request.ContentType = "application/x-www-form-urlencoded";
+            SetPostData(request, postData);
+
+            return GetResponse(request);
+        }
+
+        private void SetPostData(WebRequest request, string postData) {
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] postBytes = encoding.GetBytes(postData);
+            request.ContentLength = postBytes.Length;
+            using (Stream requestStream = request.GetRequestStream()) {
+                requestStream.Write(postBytes, 0, postBytes.Length);
+            }
+        }
+
+        private string GetResponse(WebRequest request) {
+            using (WebResponse response = request.GetResponse()) {
+                using (StreamReader responseStream = new StreamReader(response.GetResponseStream())) {
+                    return responseStream.ReadToEnd();
+                }
+            }
         }
     }
 }
