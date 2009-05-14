9ea8e55f4c644a9c79190c876aecff0a14cd77ae
Mike Henry <mhenry07@yahoo.com>
2009-05-14 02:25:53 -0500

    Rewrote TestRunner tests to use Rhino.Mocks dynamic stubs instead of my FakeProxy stub.
    Deleted FakeProxy class.

--- src/AspUnitRunner.Tests/TestRunner.cs
+++ src/AspUnitRunner.Tests/TestRunner.cs
@@ -1,47 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using NUnit.Framework;
-using AspUnitRunner;
 using System.Net;
+using NUnit.Framework;
+using Rhino.Mocks;
 
 namespace AspUnitRunner.Tests {
     [TestFixture]
     public class TestRunner {
-        private FakeProxy _fakeProxy;
+        private IProxy _proxy;
 
         [SetUp]
         public void Setup() {
-            _fakeProxy = new FakeProxy();
-            _fakeProxy.HtmlResults = FormatTestSummary(1, 0, 0);
+            _proxy = MockRepository.GenerateStub<IProxy>();
+            _proxy.Stub(x => x.GetTestResults("", "", null))
+                .IgnoreArguments()
+                .Return(FormatTestSummary(1, 0, 0));
         }
 
         [Test]
         public void Running_tests_should_return_results() {
-            Runner runner = new Runner("", _fakeProxy);
+            Runner runner = new Runner("", _proxy);
             Assert.That(runner.Run(""), Is.TypeOf<Results>());
         }
 
         [Test]
         public void Should_pass_expected_uri_to_proxy() {
-            Runner runner = new Runner("http://path/to/test-runner", _fakeProxy);
+            Runner runner = new Runner("http://path/to/test-runner", _proxy);
             Results results = runner.Run("");
-            Assert.That(_fakeProxy.Uri, Is.EqualTo("http://path/to/test-runner?UnitRunner=results"));
+            _proxy.AssertWasCalled(x => x.GetTestResults(Arg.Is("http://path/to/test-runner?UnitRunner=results"), Arg<string>.Is.Anything, Arg<ICredentials>.Is.Anything));
         }
 
         [Test]
         public void Should_pass_expected_data_to_proxy() {
-            Runner runner = new Runner("http://path/to/test-runner", _fakeProxy);
+            Runner runner = new Runner("http://path/to/test-runner", _proxy);
             Results results = runner.Run("TestContainer");
-            Assert.That(_fakeProxy.PostData, Is.EqualTo("cboTestContainers=TestContainer&cboTestCases=All+Test+Cases&cmdRun=Run+Tests"));
+            _proxy.AssertWasCalled(x => x.GetTestResults(Arg<string>.Is.Anything, Arg.Is("cboTestContainers=TestContainer&cboTestCases=All+Test+Cases&cmdRun=Run+Tests"), Arg<ICredentials>.Is.Anything));
         }
 
         [Test]
         public void Should_pass_credentials_to_proxy() {
             ICredentials credentials = new NetworkCredential("username", "password");
-            Runner runner = new Runner("", credentials, _fakeProxy);
+            Runner runner = new Runner("", credentials, _proxy);
             Results results = runner.Run("");
-            Assert.That(_fakeProxy.Credentials, Is.EqualTo(credentials));
+            _proxy.AssertWasCalled(x => x.GetTestResults(Arg<string>.Is.Anything, Arg<string>.Is.Anything, Arg.Is(credentials)));
         }
 
         public static string FormatTestSummary(int tests, int errors, int failures) {
