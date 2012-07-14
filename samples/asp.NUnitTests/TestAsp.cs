c8a5f96b126f5a974e2520a5ada3932645b687fd
Mike Henry <mhenry07@yahoo.com>
2012-07-13 18:31:08 -0700

    Clean up TestAsp sample

--- samples/asp.NUnitTests/TestAsp.cs
+++ samples/asp.NUnitTests/TestAsp.cs
@@ -4,16 +4,16 @@ using AspUnitRunner;
 namespace asp.NUnitTests {
     [TestFixture]
     public class TestAsp {
-        // set the URI for your ASPUnit tests
-        private const string AspUnitUri = "http://localhost/AspUnitRunner/asp.tests/";
+        // set the URL for your ASPUnit tests
+        private const string AspUnitUrl = "http://localhost/AspUnitRunner/asp.tests/";
 
         [Test]
         public void TestCase(
             // set ASPUnit test containers here
             [Values("CalculatorTest", "StringUtilityTest")] string testContainer
             ) {
-            Runner runner = new Runner(AspUnitUri);
-            Results results = runner.Run(testContainer);
+            var runner = new Runner(AspUnitUrl);
+            var results = runner.Run(testContainer);
 
             // Note: results.Details can generate a long HTML string which NUnit doesn't format very well
             Assert.That(results.Errors, Is.EqualTo(0), results.Details);
