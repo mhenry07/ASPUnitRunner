b1816a9f0a4f71a1cfad5f5955eedf8b83a06e60
Mike Henry <mhenry07@yahoo.com>
2012-07-26 18:31:43 -0700

    Update Ioc test to verify complete object graph

--- src/Tests/Infrastructure/TestIoc.cs
+++ src/Tests/Infrastructure/TestIoc.cs
@@ -1,14 +1,25 @@
-﻿using NUnit.Framework;
+﻿using System.Reflection;
+using NUnit.Framework;
 using AspUnitRunner.Infrastructure;
 
 namespace AspUnitRunner.Tests.Infrastructure {
-    // note that this only tests the top-most dependency
     [TestFixture]
     public class TestIoc {
         [Test]
-        public void ResolveAspProxy_should_return_AspProxy() {
+        public void ResolveAspProxy_should_return_expected_object_graph() {
             var proxy = Ioc.ResolveAspProxy();
-            Assert.That(proxy, Is.TypeOf<AspProxy>());
+            Assert.That(proxy, Is.InstanceOf<AspProxy>());
+
+            Assert.That(proxy.GetField("_webRequestFactory"),
+                Is.InstanceOf<WebRequestFactory>());
+        }
+    }
+
+    public static class ReflectionExtensions {
+        public static object GetField(this object instance, string fieldName) {
+            var fieldInfo = instance.GetType()
+                .GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+            return fieldInfo.GetValue(instance);
         }
     }
 }
