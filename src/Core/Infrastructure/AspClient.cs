24e100215075923514108b0778defaeb3e513e1d
Mike Henry <mhenry07@yahoo.com>
2012-07-27 00:38:30 -0700

    Implement AspClient which uses a WebClient

---
+++ src/Core/Infrastructure/AspClient.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Web;
+using AspUnitRunner.Core;
+
+namespace AspUnitRunner.Infrastructure {
+    internal class AspClient : IAspProxy {
+        private IWebClientFactory _factory;
+
+        public AspClient(IWebClientFactory factory) {
+            _factory = factory;
+        }
+
+        public string GetTestResults(string url, IEnumerable<KeyValuePair<string, string>> postData, ICredentials credentials) {
+            using (var webClient = _factory.Create()) {
+                webClient.Credentials = credentials;
+                webClient.Headers.Add(HttpRequestHeader.ContentType, "application/x-www-form-urlencoded");
+                return webClient.UploadString(url, FormatPostData(postData));
+            }
+        }
+
+        private string FormatPostData(IEnumerable<KeyValuePair<string, string>> postValues) {
+            var sb = new StringBuilder();
+            foreach (var value in postValues) {
+                if (sb.Length > 0)
+                    sb.Append("&");
+                sb.Append(FormatPostValue(value));
+            };
+            return sb.ToString();
+        }
+
+        private string FormatPostValue(KeyValuePair<string, string> value) {
+            return string.Format("{0}={1}",
+                HttpUtility.UrlEncode(value.Key),
+                HttpUtility.UrlEncode(value.Value));
+        }
+    }
+}
---
+++ src/Tests/Infrastructure/TestAspClient.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using NUnit.Framework;
+using Rhino.Mocks;
+using AspUnitRunner.Core;
+using AspUnitRunner.Infrastructure;
+
+namespace AspUnitRunner.Tests.Infrastructure {
+    [TestFixture]
+    public class TestAspClient {
+        [Test]
+        public void GetTestResults_should_post_data_and_return_expected_response() {
+            // Arrange
+            const string address = "http://path/to/test-runner?key=value";
+            var postValues = new KeyValuePair<string, string>[] {
+                new KeyValuePair<string, string>("key1", "value 1"),
+                new KeyValuePair<string, string>("key2", "value 2"),
+            };
+            const string postData = "key1=value+1&key2=value+2";
+            var credentials = MockRepository.GenerateStub<ICredentials>();
+            const string expectedResponse = "response";
+
+            var factory = MockRepository.GenerateStub<IWebClientFactory>();
+            var webClient = MockRepository.GenerateStub<WebClient>();
+            webClient.Headers = new WebHeaderCollection();
+            factory.Stub(f => f.Create()).Return(webClient);
+            webClient.Stub(c => c.UploadString(address, postData))
+                .Return(expectedResponse);
+
+            // Act
+            var aspClient = new AspClient(factory);
+            var response = aspClient.GetTestResults(address, postValues, credentials);
+
+            // Assert
+            Assert.That(webClient.Credentials, Is.EqualTo(credentials));
+            Assert.That(webClient.Headers[HttpRequestHeader.ContentType],
+                Is.EqualTo("application/x-www-form-urlencoded"));
+            Assert.That(response, Is.EqualTo(expectedResponse));
+        }
+    }
+}
