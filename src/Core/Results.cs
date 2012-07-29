6ed71d5a73933a9acb936914619ae8e090f58f6c
Mike Henry <mhenry07@yahoo.com>
2012-07-29 16:55:10 -0700

    Change Results constructor to internal and add documentation

--- src/Core/Results.cs
+++ src/Core/Results.cs
@@ -5,23 +5,35 @@
         private readonly int _failures;
         private readonly string _details;
 
+        /// <summary>
+        /// Gets the number of tests run.
+        /// </summary>
         public int Tests {
             get { return _tests; }
         }
 
+        /// <summary>
+        /// Gets the number of test errors.
+        /// </summary>
         public int Errors {
             get { return _errors; }
         }
 
+        /// <summary>
+        /// Gets the number of test failures.
+        /// </summary>
         public int Failures {
             get { return _failures; }
         }
 
+        /// <summary>
+        /// Gets the raw HTML test results.
+        /// </summary>
         public string Details {
             get { return _details; }
         }
 
-        public Results(int tests, int errors, int failures, string details) {
+        internal Results(int tests, int errors, int failures, string details) {
             _tests = tests;
             _errors = errors;
             _failures = failures;
