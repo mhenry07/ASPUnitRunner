d36780665e926517983b50ada30573ab913cecc1
Mike Henry <mhenry07@yahoo.com>
2012-08-15 16:28:35 -0700

    Use string.Equals instead of string.Compare

--- src/Tests/Helpers/HtmlElementEqualityComparer.cs
+++ src/Tests/Helpers/HtmlElementEqualityComparer.cs
@@ -6,7 +6,7 @@ using AspUnitRunner.Core.Html;
 namespace AspUnitRunner.Tests.Helpers {
     internal class HtmlElementEqualityComparer : IEqualityComparer<HtmlElement> {
         public bool Equals(HtmlElement x, HtmlElement y) {
-            return string.Compare(x.TagName, y.TagName, StringComparison.InvariantCultureIgnoreCase) == 0
+            return string.Equals(x.TagName, y.TagName, StringComparison.InvariantCultureIgnoreCase)
                 && x.Attributes.SequenceEqual(y.Attributes)
                 && x.InnerHtml.Equals(y.InnerHtml);
         }
