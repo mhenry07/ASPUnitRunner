1f1501da57079c741886983405005ee981b69e9d
Mike Henry <mhenry07@yahoo.com>
2012-08-19 03:34:10 -0700

    Add rake tasks to start and stop sample web site

--- rakefile.rb
+++ rakefile.rb
@@ -5,6 +5,12 @@ PACKAGES_DIR = "lib"
 NUNIT_PACKAGE = "NUnit.Runners"
 NUNIT_CONSOLE = "#{PACKAGES_DIR}/#{NUNIT_PACKAGE}/tools/nunit-console.exe"
 
+IIS_EXPRESS_EXE = "iisexpress.exe"
+IIS_EXPRESS_DIR = "IIS Express"
+WEB_SAMPLE_DIR = "sample/Web"
+WEB_PORT = "54831"
+WEB_ADDRESS = "http://localhost:#{WEB_PORT}"
+
 MAIN_SOLUTION = "src/AspUnitRunner.sln"
 CORE_PROJECT = "src/Core/AspUnitRunner.csproj"
 MAIN_TESTS = "src/Tests/AspUnitRunner.Tests.csproj"
@@ -77,5 +83,49 @@ namespace :test do
 	# note: if nuget.exe was missing, a build prereq should've grabbed it
 	file NUNIT_CONSOLE do
 		sh %{#{NUGET} install #{NUNIT_PACKAGE} -o #{PACKAGES_DIR} -x}
+	end
+end
+
+namespace :web do
+	# note that the process id from $?.pid is not the iisexpress pid
+	desc "Start the AspUnitRunner sample web site in IIS Express"
+	task :start do
+		puts "Starting the sample web site at #{WEB_ADDRESS}"
+		puts
+		command = %{"#{get_iis_express_path}" /path:"#{get_web_sample_path}" /port:#{WEB_PORT}}
+		sh %{START "" #{command}}
+	end
+
+	# find and kill the iisexpress.exe process with the expected /port command-line argument
+	desc "Stop the AspUnitRunner sample web site"
+	task :stop do
+		puts "Stopping the sample web site"
+		puts
+		process_id_text = `WMIC PROCESS WHERE "Name='#{IIS_EXPRESS_EXE}' AND CommandLine LIKE '%/port:#{WEB_PORT}%'" GET ProcessId /VALUE`
+		process_id = /(?<=ProcessId=)\d+/.match(process_id_text)
+		sh %{TASKKILL /PID #{process_id}}
+	end
+
+	def get_iis_express_path()
+		path = build_iis_express_path("ProgramFiles")
+		return path if File.exists?(path)
+
+		path = build_iis_express_path("ProgramFiles(x86)")
+		return path if File.exists?(path)
+
+		fail "Could not find IIS Express"
+	end
+
+	def build_iis_express_path(program_files_var)
+		return [ ENV[program_files_var], IIS_EXPRESS_DIR, IIS_EXPRESS_EXE ].join('\\')
+	end
+
+	# IIS Express seems to require backslashes in path
+	def get_web_sample_path()
+		return backslashify(File.join(Dir.pwd, WEB_SAMPLE_DIR))
+	end
+
+	def backslashify(path)
+		return path.gsub(/\//, '\\')
 	end
 end
