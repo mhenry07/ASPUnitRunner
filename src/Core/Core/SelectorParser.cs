bfecfe4fc421c04b36f9060e86b8392de7489632
Mike Henry <mhenry07@gmail.com>
2012-08-29 14:38:04 -0700

    Implement SelectorParser.ParseContainers

--- src/Core/Core/SelectorParser.cs
+++ src/Core/Core/SelectorParser.cs
@@ -4,6 +4,10 @@ using AspUnitRunner.Core.Html;
 
 namespace AspUnitRunner.Core {
     internal class SelectorParser : ISelectorParser {
+        private const string SelectorFormName = "frmSelector";
+        private const string TestContainersSelectName = "cboTestContainers";
+        private const string TestCasesSelectName = "cboTestCases";
+
         private readonly IHtmlDocumentFactory _htmlDocumentFactory;
 
         public SelectorParser(IHtmlDocumentFactory htmlDocumentFactory) {
@@ -11,11 +15,41 @@ namespace AspUnitRunner.Core {
         }
 
         public IEnumerable<string> ParseContainers(string html) {
-            throw new NotImplementedException();
+            var document = _htmlDocumentFactory.Create(html);
+            var selectorForm = GetSelectorForm(document);
+            var testContainersSelect = GetSelectElement(selectorForm, TestContainersSelectName);
+            return ParseSelectorOptions(testContainersSelect);
         }
 
         public IEnumerable<string> ParseTestCases(string html, string container) {
             throw new NotImplementedException();
+        }
+
+        private static IHtmlElement GetSelectorForm(IHtmlDocument document) {
+            var forms = document.GetElementsByTagName("FORM");
+            return GetFirstElementByName(forms, SelectorFormName);
+        }
+
+        private static IHtmlElement GetSelectElement(IHtmlElement form, string name) {
+            var selectElements = form.GetElementsByTagName("SELECT");
+            return GetFirstElementByName(selectElements, name);
+        }
+
+        private static IHtmlElement GetFirstElementByName(IHtmlCollection elements, string name) {
+            foreach (var element in elements)
+                if (string.Equals(element.GetAttribute("NAME"), name, StringComparison.Ordinal))
+                    return element;
+            throw new FormatException("Unexpected response.");
+        }
+
+        // ignores the first option (All Test Containers or All Test Cases)
+        private static IEnumerable<string> ParseSelectorOptions(IHtmlElement selectElement) {
+            var result = new List<string>();
+            var optionElements = HtmlElementParser.GetOptionElements(selectElement.InnerHtml);
+            foreach (var option in optionElements)
+                if (option != optionElements.First)
+                    result.Add(option.Text);
+            return result;
         }
     }
 }
---
+++ src/Tests/Unit/Core/TestSelectorParser.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+using AspUnitRunner.Core;
+using AspUnitRunner.Core.Html;
+using AspUnitRunner.Tests.Helpers;
+
+namespace AspUnitRunner.Tests.Unit.Core {
+    [TestFixture]
+    public class TestSelectorParser {
+        [Test]
+        public void ParseTestContainers_empty_should_return_empty_list() {
+            var html = FakeTestFormatter.FormatSelector(new string[] { }, null);
+
+            var parser = CreateSelectorParser(html);
+            var containers = parser.ParseContainers(html);
+            Assert.That(containers, Is.Empty);
+        }
+
+        [Test]
+        public void ParseTestContainers_should_return_expected_containers() {
+            var expectedContainers = new[] {
+                "first",
+                "second"
+            };
+            var html = FakeTestFormatter.FormatSelector(expectedContainers, null);
+
+            var parser = CreateSelectorParser(html);
+            var containers = parser.ParseContainers(html);
+            Assert.That(containers, Is.EqualTo(expectedContainers));
+        }
+
+        private SelectorParser CreateSelectorParser(string html) {
+            var htmlDocumentFactory = MockRepository.GenerateStub<IHtmlDocumentFactory>();
+            htmlDocumentFactory.Stub(f => f.Create(html))
+                .Return(new HtmlDocument(html));
+            return new SelectorParser(htmlDocumentFactory);
+        }
+    }
+}
